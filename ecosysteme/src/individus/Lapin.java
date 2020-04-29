54d6bbf95ba64f0c56153f99bc2e2c50a6c54d3f
LJeannest <lulustucru.dreamygirls@hotmail.fr>
2020-04-29 17:21:31 +0200

    suppression de la classe lapin et remplacement par la classe lievre

--- ecosysteme/src/individus/Lapin.java
+++
@@ -1,9 +0,0 @@
-package individus;
-
-public class Lapin extends Herbivore {
-
-	public Lapin(double position, String sexe, int age) {
-		super("Lapin", position, sexe, age, 20);
-	}
-
-}
---
+++ ecosysteme/src/individus/Lievre.java
@@ -0,0 +1,8 @@
+package individus;
+
+public class Lievre extends Herbivore {
+
+	public Lievre(double position, String sexe, int age) {
+		super("Lievre", position, sexe, age, 20);
+	}
+}
