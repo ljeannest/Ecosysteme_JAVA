7e0fcfc2ceb317efe4157e79d732cc0d64f979f7
LJeannest <lulustucru.dreamygirls@hotmail.fr>
2020-05-02 18:24:09 +0200

    ajout d'un package main, et d'une classe run, qui permettra de faire fonctionner le programme. Pour l'instant j'y ai inscrit les grandes étapes qu'il faudra coder pour avoir un idée plus claire de ce qu'il y aura à coder.

---
+++ ecosysteme/src/main/Run_ecosysteme.java
@@ -0,0 +1,29 @@
+package main;
+
+public class Run_ecosysteme {
+
+	public static void main(String[] args) {
+		
+		
+		//ouverture d'une premiere fenetre permettant à l'utilisateur de sélectionner les paramètres de la simulation.
+		
+		
+		//une fois les paramètres validés, la fenetre se ferme, et les paramètres sont enregistrés.
+		
+		
+		//création de l'écosystème, ainsi que des différents individus
+		
+		
+		//entrée dans une boucle qui modélise notre écosysteme, avec tous nos individus qui peuvent 
+		//boire, manger, se deplacer, se reproduire.
+		//la boucle se termine quand ? extinction d'une espece? laps de temps donné? extinction de toutes les especes?
+		
+		
+		//fermeture de l'ecosysteme.
+		
+		
+		//nouvelle fenetre affichant les statistiques de la modelisation.
+
+	}
+
+}
