757ab0bce979077848dcc55f9e64f7d1e68e4a76
augus <augus@asus-augustin-cosson-ptsi>
2020-05-07 15:51:11 +0200

    creation package temps pour update nos matrices avant affichage.
    il faut maintenant les remplir pour que ça se passe bien

---
+++ ecosysteme/src/temps/animaux.java
@@ -0,0 +1,8 @@
+package temps;
+
+public class animaux {
+
+	public int [] grille_animaux;
+	//appel fonction initialisation_grille_animaux()
+	
+}
---
+++ ecosysteme/src/temps/ressources.java
@@ -0,0 +1,26 @@
+package temps;
+
+
+
+public class ressources {
+	
+	public int [] grille_ressources;
+	
+
+	//appel fonction initialisation_grille_ressources()
+	
+	
+	private void update_position() {}
+	
+	private void update_attribut2() {}
+	
+	
+	private void update_population() {}
+	
+	private void uddate_grille_ressource() {}
+	
+	
+	
+	
+	
+}
