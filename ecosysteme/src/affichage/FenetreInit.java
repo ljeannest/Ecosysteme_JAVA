79d9fe8c3eb57f896eb841dee2be76c5dd88dc99
LJeannest <lulustucru.dreamygirls@hotmail.fr>
2020-05-08 13:32:15 +0200

    changement de lapins à lievres et ajout d'icones dans la fenetre d'initialisation des parametres de la simulation.

--- ecosysteme/src/affichage/FenetreInit.java
+++ ecosysteme/src/affichage/FenetreInit.java
@@ -3,6 +3,7 @@ package affichage;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.BorderFactory;
@@ -20,12 +21,20 @@ import javax.swing.JTextField;
 public class FenetreInit extends JDialog {
 	private FenetreInitInfo info= new FenetreInitInfo();
 	private boolean sendData;
-	private JLabel lapinLabel, lynxLabel, vautourLabel, flaqueLabel, dureeLabel,anLabel;
-	private JComboBox lapin,lynx,vautour,flaque,duree;
+	private JLabel lievreLabel, lynxLabel, vautourLabel, flaqueLabel, dureeLabel,anLabel;
+	private JComboBox lievre,lynx,vautour,flaque,duree;
+	private JLabel lievreIcon,lynxIcon,vautourIcon,flaqueIcon,tempsIcon;
+	
+
+	ImageIcon icon_eau = new ImageIcon(new ImageIcon("images/bleu.jpg").getImage().getScaledInstance(50,50, Image.SCALE_DEFAULT));
+	ImageIcon icon_lynx = new ImageIcon(new ImageIcon("images/lynx.jpg").getImage().getScaledInstance(50,50, Image.SCALE_DEFAULT));
+	ImageIcon icon_lievre = new ImageIcon(new ImageIcon("images/lievre.jpg").getImage().getScaledInstance(50,50, Image.SCALE_DEFAULT));
+	ImageIcon icon_vautour = new ImageIcon(new ImageIcon("images/vautour.jpg").getImage().getScaledInstance(50,50, Image.SCALE_DEFAULT));
+	ImageIcon icon_temps = new ImageIcon(new ImageIcon("images/temps.jpg").getImage().getScaledInstance(50, 50, Image.SCALE_DEFAULT));
 	
 	public FenetreInit(JFrame parent,String titre, boolean modal) {
 		super(parent, titre, modal);
-		this.setSize(700,250);
+		this.setSize(1000,300);
 		this.setLocationRelativeTo(null);
 		this.setResizable(false);
 		this.setDefaultCloseOperation(JDialog.DO_NOTHING_ON_CLOSE);
@@ -43,42 +52,48 @@ public class FenetreInit extends JDialog {
 		// creation du panel individus
 		JPanel pan_individus = new JPanel();
 		pan_individus.setBackground(Color.white);
-		pan_individus.setPreferredSize(new Dimension(650,80));
+		pan_individus.setPreferredSize(new Dimension(900,90));
 		pan_individus.setBorder(BorderFactory.createTitledBorder("Individus"));
 		
-		//lapins
+		//lievres
+		lievreIcon=new JLabel(icon_lievre);
 		JPanel pan_lapin= new JPanel();
 		pan_lapin.setBackground(Color.white);
-		pan_lapin.setPreferredSize(new Dimension(190,80));
-		lapin=new JComboBox();
+		pan_lapin.setPreferredSize(new Dimension(290,80));
+		lievre=new JComboBox();
 		for (int k=1;k<=20;k++) {
-			lapin.addItem(k);
+			lievre.addItem(k);
 		}
-		lapinLabel=new JLabel("Nombre de lapins : ");
-		pan_lapin.add(lapinLabel);
-		pan_lapin.add(lapin);
+		lievreLabel=new JLabel("Nombre de lievres : ");
+		pan_lapin.add(lievreIcon);
+		pan_lapin.add(lievreLabel);
+		pan_lapin.add(lievre);
 		
 		//lynx
+		lynxIcon=new JLabel(icon_lynx);
 		JPanel pan_lynx= new JPanel();
 		pan_lynx.setBackground(Color.white);
-		pan_lynx.setPreferredSize(new Dimension(190,80));
+		pan_lynx.setPreferredSize(new Dimension(290,80));
 		lynx=new JComboBox();
 		for (int k=1;k<=30;k++) {
 			lynx.addItem(k);
 		}
 		lynxLabel=new JLabel("Nombre de lynx : ");
+		pan_lynx.add(lynxIcon);
 		pan_lynx.add(lynxLabel);
 		pan_lynx.add(lynx);
 		
 		//vautour
+		vautourIcon=new JLabel(icon_vautour);
 		JPanel pan_vautour= new JPanel();
 		pan_vautour.setBackground(Color.white);
-		pan_vautour.setPreferredSize(new Dimension(190,80));
+		pan_vautour.setPreferredSize(new Dimension(290,80));
 		vautour=new JComboBox();
 		for (int k=1;k<=10;k++) {
 			vautour.addItem(k);
 		}
 		vautourLabel=new JLabel("Nombre de vautours : ");
+		pan_vautour.add(vautourIcon);
 		pan_vautour.add(vautourLabel);
 		pan_vautour.add(vautour);
 		
@@ -91,10 +106,11 @@ public class FenetreInit extends JDialog {
 		//creation du panel environnement
 		JPanel pan_environnement=new JPanel();
 		pan_environnement.setBackground(Color.white);
-		pan_environnement.setPreferredSize(new Dimension(325,80));
+		pan_environnement.setPreferredSize(new Dimension(450,90));
 		pan_environnement.setBorder(BorderFactory.createTitledBorder("Environnement"));
 		
 		//Nombre de flaques
+		flaqueIcon = new JLabel(icon_eau);
 		flaque=new JComboBox();
 		for (int k=4; k<=10;k++) {
 			flaque.addItem(k);
@@ -102,6 +118,7 @@ public class FenetreInit extends JDialog {
 		flaqueLabel=new JLabel ("Nombre de flaques : ");
 		
 		//Remplissage du panel environnement
+		pan_environnement.add(flaqueIcon);
 		pan_environnement.add(flaqueLabel);
 		pan_environnement.add(flaque);
 		
@@ -109,10 +126,11 @@ public class FenetreInit extends JDialog {
 		//creation du panel simulation
 		JPanel pan_simulation= new JPanel();
 		pan_simulation.setBackground(Color.white);
-		pan_simulation.setPreferredSize(new Dimension(325,80));
+		pan_simulation.setPreferredSize(new Dimension(450,90));
 		pan_simulation.setBorder(BorderFactory.createTitledBorder("Simulation"));
 		
 		//duree de la simulation
+		tempsIcon = new JLabel(icon_temps);
 		duree=new JComboBox();
 		for (int k=2;k<=100;k++) {
 			duree.addItem(k);
@@ -121,6 +139,7 @@ public class FenetreInit extends JDialog {
 		anLabel=new JLabel (" ans");
 		
 		//Remplissage du panel Simulation
+		pan_simulation.add(tempsIcon);
 		pan_simulation.add(dureeLabel);
 		pan_simulation.add(duree);
 		pan_simulation.add(anLabel);
@@ -139,7 +158,7 @@ public class FenetreInit extends JDialog {
 		JButton okBouton = new JButton("OK");
 		okBouton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				info = new FenetreInitInfo((int)lapin.getSelectedItem(),(int)lynx.getSelectedItem(),(int)vautour.getSelectedItem(),(int)flaque.getSelectedItem(),(int)duree.getSelectedItem());
+				info = new FenetreInitInfo((int)lievre.getSelectedItem(),(int)lynx.getSelectedItem(),(int)vautour.getSelectedItem(),(int)flaque.getSelectedItem(),(int)duree.getSelectedItem());
 				setVisible(false);
 			}
 		});
--- ecosysteme/src/affichage/FenetreInitInfo.java
+++ ecosysteme/src/affichage/FenetreInitInfo.java
@@ -2,12 +2,12 @@ package affichage;
 
 public class FenetreInitInfo {
 	
-	int nb_lapin,nb_lynx,nb_vautour,nb_flaques,duree;
+	int nb_lievre,nb_lynx,nb_vautour,nb_flaques,duree;
 	
 	public FenetreInitInfo() {}
 	
-	public FenetreInitInfo(int nb_lapin,int nb_lynx,int nb_vautour,int nb_flaques, int duree) {
-		this.nb_lapin=nb_lapin;
+	public FenetreInitInfo(int nb_lievre,int nb_lynx,int nb_vautour,int nb_flaques, int duree) {
+		this.nb_lievre=nb_lievre;
 		this.nb_lynx=nb_lynx;
 		this.nb_vautour=nb_vautour;
 		this.nb_flaques=nb_flaques;
@@ -16,9 +16,9 @@ public class FenetreInitInfo {
 	
 	public String toString() {
 		String str;
-		if(this.nb_lapin!=0 && this.nb_lynx!=0 && this.nb_vautour!=0 && this.nb_flaques!=0 && this.duree!=0){
+		if(this.nb_lievre!=0 && this.nb_lynx!=0 && this.nb_vautour!=0 && this.nb_flaques!=0 && this.duree!=0){
 			str = "Verification des parametres \n\n\n";
-			str+= "Nombre de lapins : "+this.nb_lapin +"\n";
+			str+= "Nombre de lievres : "+this.nb_lievre +"\n";
 			str+= "Nombre de lynxs : "+this.nb_lynx +"\n";
 			str+= "Nombre de vautours : "+this.nb_vautour +"\n\n";
 			str+= "Nombre de flaques : "+this.nb_flaques +"\n\n";
