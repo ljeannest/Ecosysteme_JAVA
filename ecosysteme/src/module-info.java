7df6e57558defb1accb8c45df12dee8321b1458e
Mélodia <Mlodia@LAPTOP-16CNDUDV>
2020-05-12 11:14:29 +0200

    importation d'une nouvelle librairie

---
+++ ecosysteme/src/module-info.java
@@ -0,0 +1,14 @@
+module ecosysteme {
+	exports affichage;
+	exports Reproduction;
+	exports statistique;
+	exports main;
+	exports individus;
+	exports ressources;
+
+	requires io;
+	requires java.desktop;
+	requires kernel;
+	requires layout;
+	requires jfreechart;
+}
