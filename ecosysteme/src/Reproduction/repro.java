bb7bc3ea9a06e884a1b89da6715b988585da710d
LJeannest <lulustucru.dreamygirls@hotmail.fr>
2020-05-13 15:27:27 +0200

    modif tableau animaux reproduction

--- ecosysteme/src/Reproduction/repro.java
+++ ecosysteme/src/Reproduction/repro.java
@@ -3,16 +3,17 @@ package Reproduction;
 import individus.Animal;
 import ressources.Vegetaux;
 import java.lang.Math;
+import java.util.ArrayList;
 
 public class repro {
 
-	public static boolean placeVide(int posx, int posy, Animal tableauIndividu[], Vegetaux tableauRessource[]){
+	public static boolean placeVide(int posx, int posy, ArrayList<Animal> A, Vegetaux tableauRessource[]){
 
 		boolean placeLibre = true;
 
-		for(int i = 0; i < tableauIndividu.length; i++) {
+		for(int i = 0; i < A.size(); i++) {
 
-			if (tableauIndividu[i].posx == posx && tableauIndividu[i].posy == posy) {
+			if (A.get(i).posx == posx && A.get(i).posy == posy) {
 
 				placeLibre=false;
 
@@ -50,25 +51,25 @@ public class repro {
 
 	}
 	
-	public static int [] vérificationNouvellePlace(int posx, int posy,Animal tableauIndividu[],Vegetaux tableauRessource[]) {
+	public static int [] vérificationNouvellePlace(int posx, int posy,ArrayList<Animal> A,Vegetaux tableauRessource[]) {
 		// on vérifie avec cette fonction que le nouveau né peut être placé prés de sa mére
 		
 		int posX;
 		int posY;
 		
-		if (placeVide(posx+1, posy,tableauIndividu,tableauRessource)==true) {
+		if (placeVide(posx+1, posy,A,tableauRessource)==true) {
 			posX = posx+1;
 			posY = posy;
 		}
-		else if (placeVide(posx-1,posy,tableauIndividu,tableauRessource)==true) {
+		else if (placeVide(posx-1,posy,A,tableauRessource)==true) {
 			posX = posx-1;
 			posY = posy;
 		}
-		else if(placeVide(posx,posy+1,tableauIndividu,tableauRessource)==true) {
+		else if(placeVide(posx,posy+1,A,tableauRessource)==true) {
 			posX = posx+1;
 			posY = posy;
 		}
-		else if(placeVide(posx,posy-1,tableauIndividu,tableauRessource)==true) {
+		else if(placeVide(posx,posy-1,A,tableauRessource)==true) {
 			posX = posx+1;
 			posY = posy;
 		}
@@ -83,7 +84,7 @@ public class repro {
 	}
 
 
-	public static Animal créationNouvelIndividu(Animal A1, Animal A2, Animal tableauIndividu[], Vegetaux tableauRessource[]) {
+	public static void créationNouvelIndividu(Animal A1, Animal A2, ArrayList<Animal> A, Vegetaux tableauRessource[]) {
 
 		String sexe;
 
@@ -100,15 +101,15 @@ public class repro {
 		int positionNaissance[];
 		// on crée les coordonné du nouvel animal qu'on place auprés de sa mére par défault
 		if(A1.sexe == "F") {
-			positionNaissance = vérificationNouvellePlace(A1.posx, A1.posy,tableauIndividu,tableauRessource);
+			positionNaissance = vérificationNouvellePlace(A1.posx, A1.posy,A,tableauRessource);
 		}
 		else {
-			positionNaissance = vérificationNouvellePlace(A2.posx, A2.posy,tableauIndividu,tableauRessource);
+			positionNaissance = vérificationNouvellePlace(A2.posx, A2.posy,A,tableauRessource);
 		}
 
 		Animal A3= new Animal(A1.espece, A1.type, positionNaissance[0], positionNaissance[1], sexe, 0, A1.esp_de_vie, 100, 100, A1.qte_viande, A1.ageReproMin, A2.ageReproMax );
 		
-		return A3;
+		A.add(A3);
 	}
 
 
