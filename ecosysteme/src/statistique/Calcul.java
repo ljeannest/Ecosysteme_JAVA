c430cdab4adc4309b38581f0664c20008592e464
Mélodia <Mlodia@LAPTOP-16CNDUDV>
2020-05-15 09:18:34 +0200

    methode calcul nbIndividus plus doc

--- ecosysteme/src/statistique/Calcul.java
+++ ecosysteme/src/statistique/Calcul.java
@@ -17,17 +17,17 @@ import individus.*;
  */
 
 public class Calcul {
-	
+
 	/**
 	 * Liste contenant les données des individus vivant à la fin de la simulation.
 	 */
 	ArrayList<Animal> individus;
-	
+
 	/**
 	 * Liste contenant les données des individus mort à la fin de la simulation.
 	 */
 	ArrayList<Animal> individus_morts;
-	
+
 	/**
 	 * Constructeur de la classe Calcul
 	 * 
@@ -36,22 +36,64 @@ public class Calcul {
 	 * @param individus_morts
 	 * 			Array liste de type animal.
 	 */
-	
+
 	public Calcul(ArrayList<Animal> individus, ArrayList<Animal> individus_morts) {
-		
-		
+
+
 		this.individus = individus;
 		this.individus_morts = individus_morts;
 	}
-	
+
 	/**
 	 * Permet de compter le nombre d'individus par espece présent durant toute la simulation.
+	 * @return 
+	 * 
+	 * @return Une liste de type int contenant dans l'ordre suivant le nombre d'individus présent lors de toute la simulation: Herbivore, Carnivore, Charognard.
 	 */
-	public compteurNbIndividus() {
+	public int[] compteurNbIndividus() {
+
+		int [] nbIndividus = new int [3];
+		for(int i=0; i<individus.size(); i++) {
+
+			if (individus.get(i).type==0) {
+
+				nbIndividus[0]+=1;
+
+			}
+			else if (individus.get(i).type==1) {
+
+				nbIndividus[1]+=1;
+
+			}
+			else {
+				nbIndividus[2]+=1;
+			}
+		}
+		for(int j=0; j<individus_morts.size();j++) {
+			
+			if(individus_morts.get(j).type==0) {
+				
+				nbIndividus[0]+=1;
+			}
+			
+			else if (individus_morts.get(j).type==1) {
+				
+				nbIndividus[1]+=1;
+				
+			}
+			
+			else {
+				
+				nbIndividus[2]+=1;
+				
+			}
 		
-		
-	}
-	
-	
+		}
+		return nbIndividus;
+
+		}
+
+
+
 
 }
